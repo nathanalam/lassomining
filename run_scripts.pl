3c7cca97186b676888c10dd976c07af272a38128
nathanalam <nathanalam99@gmail.com>
2019-09-19 23:46:51 -0400

    jupyter code

--- run_scripts.pl
+++ run_scripts.pl
@@ -47,25 +47,25 @@ sub readFASTA {
 	my @seql;
 	# print "\n opening $name \n";
 	if (open (FASTA, "<$name")) { 
-	my $count = -1;
-	while (my $myline = <FASTA>) {
-		chomp $myline;
-		if ($myline =~ m/^>/) {
-			$count ++;
-			$prot[$count] = $myline;
-			$seq[$count] = "";
-		} else {
-			if ($cleanspace) {$myline =~ s/\s//gi;} 
-			if ($count < 0) {print "error: $myline \n";}
-			$seq[$count] .= $myline;
+		my $count = -1;
+		while (my $myline = <FASTA>) {
+			chomp $myline;
+			if ($myline =~ m/^>/) {
+				$count ++;
+				$prot[$count] = $myline;
+				$seq[$count] = "";
+			} else {
+				if ($cleanspace) {$myline =~ s/\s//gi;} 
+				if ($count < 0) {print "error: $myline \n";}
+				$seq[$count] .= $myline;
+			}
 		}
+		close FASTA;
+		foreach (0..$#seq) {
+			$seql[$_] = length($seq[$_]);
+		}
+		return \@prot, \@seq, \@seql;
 	}
-	close FASTA;
-	foreach (0..$#seq) {
-		$seql[$_] = length($seq[$_]);
-	}
-	return \@prot, \@seq, \@seql;
-}
 	else {return '0', '0', '0';}
 }
 
