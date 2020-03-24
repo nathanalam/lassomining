93d6ffbee2e67f962d9e2eaa7dce26c6cde6f93b
Nathan <nathanalam99@gmail.com>
2020-03-23 23:58:50 -0400

    added automated meme generation

--- Motifmaker.py
+++ Motifmaker.py
@@ -1,6 +1,28 @@
 import os
+import shutil
 
-os.system("meme -nmotifs 4 -maxw 25 modelProteins/BProteins.faa -o BMemeResults")
-os.system("meme -nmotifs 3 -maxw 25 modelProteins/CProteins.faa -o CMemeResults")
+# Generate MEME output file from a sequence of proteins
+# @ Param - protSeq is a list of tuples, with name sequence pairs
+def makeMeme(protSeq, memeName):
+    with open("modelProteins/" + memeName + ".faa", "w") as f:
+        for seq in protSeq:
+            print("writing " + str(seq))
+            f.write(">" + seq[0] + "\n" + seq[1] + "\n\n")
+    
+    command = "meme -nmotifs " + str(len(protSeq)) + " -maxw 25 modelProteins/" + memeName + ".faa -o motifs/" + memeName
+    print(command)
+    os.system(command)
+
+    os.remove("modelProteins/" + memeName + ".faa")
+    os.rename("motifs/" + memeName + "/meme.txt", "motifs/" + memeName + "Results.txt")
+    shutil.rmtree("motifs/" + memeName)
+
+# seeq = [
+#     ("lmao", "AFAFAFAFAAFAAFAFAFAFAAFAAFAFAFAFAAFAAFAFAFAFAAFAAFAFAFAFAAFAAFAFAFAFAAFA"),
+#     ("asdf", "BSNSNSNSNBSNSNSNSNBSNSNSNSNBSNSNSNSNBSNSNSNSNBSNSNSNSNBSNSNSNSNBSNSNSNSN"),
+#     ("qwer", "REGREBERREGREBERREGREBERREGREBERREGREBERREGREBERREGREBERREGREBERREGREBER")
+# ]
+
+# makeMeme(seeq, "test")
 
 
