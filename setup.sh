22dbf4e41dfe81c0c7c0e3b90c60470b53632e45
Nathan S. Alam <nalam@della5.princeton.edu>
2020-04-27 15:54:18 -0400

    update readme

---
+++ setup.sh
@@ -0,0 +1,7 @@
+source /home/nalam/lassomining/bin/activate
+pip3 install django
+pip3 install biopython
+pip3 install pandas
+source /home/nalam/.bash_profile
+
+echo FINISHED INSTALLING PYTHON LIBRARIES
